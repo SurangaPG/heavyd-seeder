987c2510adf7f8a940bad2c05c50b68f2ef9b0b9
Suranga Panagamuwa Gamage <suranga.gamage@one-agency.be>
2018-01-11 08:48:48 +0100

    update the etc folders

--- etc/drupal8/bootstrap-files/etc/env/level27/settings.env.php
+++ etc/drupal8/bootstrap-files/etc/env/level27/settings.env.php
@@ -1,19 +1,25 @@
 <?php
 
-/**
- * @file includes the needed settings form for a level27 server.
- */
+// Generate a dummy local hash
+$dummy_hash = [];
+exec('whoami', $dummy_hash);
+$settings['hash_salt'] = base64_encode($dummy_hash[0] . __DIR__);
 
+// Read the machine name from the current dir
+$baseSiteName = basename(__DIR__);
+
+/*
+ * Read all the other data from the db properties.
+ */
 $envFile = dirname(dirname(dirname(__DIR__))) . '/.env';
 
 // Get the files from the .env file
 $envConf = base64_decode(file_get_contents($envFile));
 $envConf = json_decode($envConf, TRUE);
+$siteConf = $envConf[$baseSiteName];
 
-foreach ($envConf['database'] as $joinedKey => $data) {
-  $keyParts = explode('--', $joinedKey);
-
-  $databases[$keyParts[0]][$keyParts[1]] = $data;
+if (isset($siteConf['databases'])) {
+  $databases = $siteConf['databases'];
 }
 
-$settings['hash_salt'] = $envConf['hash_salt'];
+$settings['hash_salt'] = $siteConf['hash_salt'];
