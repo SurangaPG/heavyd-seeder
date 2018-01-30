a7e02d52a7c860700f23041c535e1fab336a46e1
Suranga Panagamuwa Gamage <suranga.gamage@one-agency.be>
2018-01-30 10:14:39 +0100

    add various tweaks to better accomodate the docker builds

---
+++ etc/drupal8/bootstrap-files/etc/env/docker/settings.env.php
@@ -0,0 +1,13 @@
+<?php
+
+$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'database' => 'web_db',
+  'username' => 'admin',
+  'password' => 'KEVSsuhdkHUdsBMdqKJSdsBhhdj3',
+  'host' => 'db',
+  'prefix' => '',
+);
+
+$settings['hash_salt'] = base64_encode(__DIR__);
+
