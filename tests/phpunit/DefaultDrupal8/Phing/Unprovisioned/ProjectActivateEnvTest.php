28a992a14998720d6ffb007ce5e9cd0741d80ec8
Suranga Panagamuwa Gamage <suranga.gamage@one-agency.be>
2018-02-28 18:34:27 +0100

    extend tests

--- tests/phpunit/DefaultDrupal8/Phing/Unprovisioned/ProjectActivateEnvTest.php
+++ tests/phpunit/DefaultDrupal8/Phing/Unprovisioned/ProjectActivateEnvTest.php
@@ -28,15 +28,58 @@ class ProjectActivateEnvTest extends AbstractDefaultDrupal8BaseTestCase {
   }
 
   /**
-   * Checks or an environment fails to activate when the env is missing.
+   * Checks or an the docker env is activated correctly.
    */
-  public function testActivateEnvProd() {
+  public function testActivateEnvDocker() {
     $return = $this->projectRunPhing('project:activate-env -Denv.to.activate=docker', $output);
     $this->assertCommandSuccessful($return, 'Failed to activate the environment.');
 
     // Check that the services.stage.yml file does not exist.
     $this->assertFileEquals($this->getProjectDirectory() . '/etc/env/docker/settings.env.php', $this->getProjectDirectory() . '/web/sites/default/settings.env.php');
 
+    // Ensure all the property files are correct.
+    $this->assertFileEquals($this->getProjectDirectory() . '/etc/env/docker/properties/docker.yml', $this->getProjectDirectory() . '/properties/env/docker.yml');
+    $this->assertFileEquals($this->getProjectDirectory() . '/etc/env/docker/properties/host.yml', $this->getProjectDirectory() . '/properties/env/host.yml');
+    $this->assertFileEquals($this->getProjectDirectory() . '/etc/env/docker/properties/project.yml', $this->getProjectDirectory() . '/properties/env/project.yml');
+
+    // Validate the count of items.
+    $items = glob($this->getProjectDirectory() . '/properties/env/*.yml');
+    $this->assertEquals(3, count($items), 'Incorrect number of items found.');
   }
 
+  /**
+   * Checks or an the docker env is activated correctly.
+   */
+  public function testActivateEnvEnvFile() {
+    $return = $this->projectRunPhing('project:activate-env -Denv.to.activate=env-file', $output);
+    $this->assertCommandSuccessful($return, 'Failed to activate the environment.');
+
+    // Check that the services.stage.yml file does not exist.
+    $this->assertFileEquals($this->getProjectDirectory() . '/etc/env/env-file/settings.env.php', $this->getProjectDirectory() . '/web/sites/default/settings.env.php');
+
+    // Ensure all the property files are correct.
+    $this->assertFileEquals($this->getProjectDirectory() . '/etc/env/env-file/properties/project.yml', $this->getProjectDirectory() . '/properties/env/project.yml');
+
+    // Validate the count of items.
+    $items = glob($this->getProjectDirectory() . '/properties/env/*.yml');
+    $this->assertEquals(1, count($items), 'Incorrect number of items found.');
+  }
+
+  /**
+   * Checks or an the docker env is activated correctly.
+   */
+  public function testActivateEnvLocalhost() {
+    $return = $this->projectRunPhing('project:activate-env -Denv.to.activate=localhost', $output);
+    $this->assertCommandSuccessful($return, 'Failed to activate the environment.');
+
+    // Check that the services.stage.yml file does not exist.
+    $this->assertFileEquals($this->getProjectDirectory() . '/etc/env/localhost/settings.env.php', $this->getProjectDirectory() . '/web/sites/default/settings.env.php');
+
+    // Ensure all the property files are correct.
+    $this->assertFileEquals($this->getProjectDirectory() . '/etc/env/localhost/properties/project.yml', $this->getProjectDirectory() . '/properties/env/project.yml');
+
+    // Validate the count of items.
+    $items = glob($this->getProjectDirectory() . '/properties/env/*.yml');
+    $this->assertEquals(3, count($items), 'Incorrect number of items found.');
+  }
 }
